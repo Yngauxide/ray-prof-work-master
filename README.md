77588a5d8cd0fd1585fa55e80c82ab370a67b0f4
Yngauxide <danieloxide4all@gmail.com>
2023-10-26 17:42:58 -0700

    ray update

---
+++ README.md
@@ -0,0 +1,5 @@
+# ray-prof-
+
+## delete all old files
+
+### old files removed
---
+++ more.js
@@ -0,0 +1,14 @@
+const images = document.querySelectorAll(".slideshow img");
+let currentImage = 0;
+
+function crossfade() {
+    images[currentImage].classList.remove("active");
+    currentImage = (currentImage + 1) % images.length;
+    images[currentImage].classList.add("active");
+}
+
+// Initial setup
+images[currentImage].classList.add("active");
+
+// Start the crossfade loop
+setInterval(crossfade, 3000); // Change image every 3 seconds
